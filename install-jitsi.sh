93c64aa5377f74e951284ee8c25fcf1ac79f9f2a
Vasyl B <xrescon@gmail.com>
2020-04-01 04:48:44 +0300

    Updated new script with successful installation.

--- install-jitsi.sh
+++ install-jitsi.sh
@@ -46,10 +46,11 @@ echo "===================> Start installation required packages <===============
 apt install -y unzip git curl make default-jdk maven dpkg wget
 # ufw
 
-echo "===================> Start installation of JitsiMeet on $DOMAIN <==================="
+echo "===================> Start installation JitsiMeet <==================="
 
 if [ ! -d "$INSTALLPATH/$DOMAIN" ]
 then
+    mkdir $INSTALLPATH
     cd $INSTALLPATH
     echo "Download and install jitsi from jitsi-meet"
     git clone https://github.com/AgenaTrader/jitsi-meet.git & wait
@@ -63,53 +64,25 @@ then
     sed -i "s/bridge: 'jitsi-videobridge.choop.chat',/\/\/ bridge: 'jitsi-videobridge.$DOMAIN',/gi" "./$DOMAIN-config.js"
     sed -i "s/muc: 'conference.choop.chat'/muc: 'conference.$DOMAIN'/gi" "./$DOMAIN-config.js"
 
-    cd $INSTALLPATH
-    sudo mv jitsi-meet ./$DOMAIN
+    sudo mv $INSTALLPATH/jitsi-meet ./$DOMAIN
 fi
 
-echo "===================> Start installation prosody <==================="
+echo "===================> Write domain to host file <==================="
 
-if command -v prosodyctl > /dev/null;
+if [ -n "$(grep $DOMAIN /etc/hosts)" ]
 then
-        echo "Prosody already installed"
+    echo "$DOMAIN already exists"
 else
-    apt install -y prosody
-fi
+    echo "Adding domain $DOMAIN to your /ets/hosts";
+    echo "127.0.0.1\t$DOMAIN" | sudo tee -a /etc/hosts
 
-echo "Prosody add new VirtualHost and Component"
-sudo cp "$INSTALLPATH/$DOMAIN/doc/example-config-files/prosody.cfg.lua.example" /etc/prosody/prosody.cfg.lua
-echo "Include \"conf.d/*.cfg.lua\"" >> /etc/prosody/prosody.cfg.lua
-service prosody restart
-
-if [ ! -f $PROSODYCONFIGPATH ]
-then
-    sudo cp "$INSTALLPATH/$DOMAIN/doc/debian/jitsi-meet-prosody/prosody.cfg.lua-jvb.example" /etc/prosody/conf.avail/$DOMAIN.cfg.lua
-
-    sudo sed -i "s/plugin_paths = { \"\/usr\/share\/jitsi-meet\/prosody-plugins\/\" }/plugin_paths = { \"\/srv\/$DOMAIN\/resources\/prosody-plugins\/\" }/g" $PROSODYCONFIGPATH
-    sudo sed -i "s/__turnSecret__/$PROSODYPASSWORD/g" $PROSODYCONFIGPATH
-    sudo sed -i "s/focusSecret/$PROSODYPASSWORD/g" $PROSODYCONFIGPATH
-    sudo sed -i "s/focusUser/focus/g" $PROSODYCONFIGPATH
-    sudo sed -i "s/jitmeet.example.com/$DOMAIN/g" $PROSODYCONFIGPATH
-
-    sudo ln -s $PROSODYCONFIGPATH /etc/prosody/conf.d/$DOMAIN.cfg.lua
-
-    echo "===================> Prosody generate keys for domain $DOMAIN and restart <==================="
-#    sudo prosodyctl cert key $DOMAIN 2048
-#    sudo prosodyctl cert key "auth.$DOMAIN" 2048
-
-    sudo prosodyctl cert generate $DOMAIN
-    sudo prosodyctl cert generate "auth.$DOMAIN"
-
-    ln -sf /var/lib/prosody/$DOMAIN.* /etc/prosody/cert/
-    ln -sf /var/lib/prosody/auth.$DOMAIN.* /etc/prosody/cert/
-
-    ln -sf /var/lib/prosody/auth.$DOMAIN.crt /usr/local/share/ca-certificates/auth.$DOMAIN.crt
-    update-ca-certificates -f
-    prosodyctl register focus auth.$DOMAIN $PROSODYPASSWORD
-    prosodyctl register jvb auth.$DOMAIN $PROSODYPASSWORD
-
-    service prosody restart
-    prosodyctl start
+    if [ -n "$(grep $DOMAIN /etc/hosts)" ]
+    then
+        echo "$DOMAIN was added succesfully";
+    else
+        echo "Failed to add $DOMAIN, try again!";
+        exit
+    fi
 fi
 
 echo "===================> Start installation nginx <==================="
@@ -123,14 +96,8 @@ else
     sudo service nginx start
 
     sudo ufw app list
-#    sudo ufw allow 'Nginx HTTP'
-#    sudo ufw allow 'Nginx HTTPS'
     sudo ufw status
 fi
-
-#sed -i "s/# tcp_nopush on;/tcp_nopush on;/gi" /etc/nginx/nginx.conf
-#sed -i "s/# types_hash_max_size 2048;/types_hash_max_size 2048;/gi" /etc/nginx/nginx.conf
-#sed -i "s/# server_names_hash_bucket_size 64;/server_names_hash_bucket_size 64;/gi" /etc/nginx/nginx.conf
 
 echo "===================> Start installation node <==================="
 
@@ -154,51 +121,39 @@ else
     curl -L https://npmjs.org/install.sh | sudo sh &
 fi
 
-if [ ! -f /etc/nginx/sites-available/$DOMAIN.conf ]
+echo "===================> Start installation prosody <==================="
+
+if command -v prosodyctl > /dev/null;
 then
-    cp -rf "$INSTALLPATH/$DOMAIN/doc/debian/jitsi-meet/jitsi-meet.example" $NGINXCONFIGPATH
-
-    sed -i "s/ssl_certificate \/etc\/jitsi\/meet\/jitsi-meet.example.com.crt;/ssl_certificate \/etc\/prosody\/certs\/$DOMAIN.crt;/g" $NGINXCONFIGPATH
-    sed -i "s/ssl_certificate_key \/etc\/jitsi\/meet\/jitsi-meet.example.com.key;/ssl_certificate_key \/etc\/prosody\/certs\/$DOMAIN.key;/g" $NGINXCONFIGPATH
-    sed -i "s/server_names_hash_bucket_size 64;/#server_names_hash_bucket_size 64;/g" $NGINXCONFIGPATH
-    sed -i "s/jitsi-meet.example.com/$DOMAIN/g" $NGINXCONFIGPATH
-    sed -i "s/\/usr\/share\/jitsi-meet/\/srv\/$DOMAIN/g" $NGINXCONFIGPATH
-    sed -i "s/\/etc\/jitsi\/meet/\/srv\/$DOMAIN/g" $NGINXCONFIGPATH
-    sed -i "s/libs\/external_api.min.js;/modules\/API\/external\/external_api.js;/g" $NGINXCONFIGPATH
-    sed -i "s/# server_names_hash_bucket_size 64;/server_names_hash_bucket_size 64;/gi" $NGINXCONFIGPATH
-
-    sudo ln -s $NGINXCONFIGPATH /etc/nginx/sites-enabled/$DOMAIN.conf
-fi
-
-echo "===================> Write domain to host file <==================="
-
-if [ -n "$(grep $DOMAIN /etc/hosts)" ]
-then
-    echo "$DOMAIN already exists"
+        echo "Prosody already installed"
 else
-    echo "Adding domain $DOMAIN to your /ets/hosts";
-    echo "127.0.0.1\t$DOMAIN" | sudo tee -a /etc/hosts
+    apt install -y jitsi-meet-prosody
 
-    if [ -n "$(grep $DOMAIN /etc/hosts)" ]
-    then
-        echo "$DOMAIN was added succesfully";
-    else
-        echo "Failed to add $DOMAIN, try again!";
-        exit
-    fi
+    echo "Prosody add new VirtualHost and Component"
+    #sudo cp "$INSTALLPATH/$DOMAIN/doc/example-config-files/prosody.cfg.lua.example" /etc/prosody/prosody.cfg.lua
+    echo "Include \"conf.d/*.cfg.lua\"" >> /etc/prosody/prosody.cfg.lua
+    service prosody restart
 fi
 
 echo "===================> Install VideoBridge <==================="
-
 cd ~
 
 if [ ! -f /etc/jitsi/videobridge ]
 then
-  apt-get update -y && apt-get upgrade -y
+  apt-get install -y jitsi-videobridge
+  apt-get update -y && apt-get upgrade -y && wait
   apt-get install -y gnupg2 apt-transport-https lsof vim jq
+
+  # To support larger number of participants we need to increase some numbers
+tee -a /etc/systemd/system.conf << EOF
+DefaultLimitNOFILE=65000
+DefaultLimitNPROC=65000
+DefaultTasksMax=65000
+EOF
+
   echo 'deb https://download.jitsi.org stable/' >> /etc/apt/sources.list.d/jitsi-stable.list
   wget -qO -  https://download.jitsi.org/jitsi-key.gpg.key | apt-key add -
-  apt-get update -y
+  apt-get update -y && wait
   echo "jitsi-videobridge2 jitsi-videobridge/jvb-hostname string $DOMAIN" | debconf-set-selections
   apt-get install -y jitsi-videobridge2=2.1-157-g389b69ff-1
 fi
@@ -211,7 +166,6 @@ sed -i "/JVB_OPTS=/c\JVB_OPTS=\"--apis=,\"" /etc/jitsi/videobridge/config
 
 echo "===================> VIDEOBRIDGE change sip-communicator.properties  <==================="
 echo "
-org.jitsi.videobridge.AUTHORIZED_SOURCE_REGEXP=focus@auth.$DOMAIN/.*
 org.ice4j.ice.harvest.DISABLE_AWS_HARVESTER=true
 org.ice4j.ice.harvest.STUN_MAPPING_HARVESTER_ADDRESSES=meet-jit-si-turnrelay.jitsi.net:443
 org.jitsi.videobridge.ENABLE_STATISTICS=true
@@ -225,13 +179,11 @@ org.jitsi.videobridge.xmpp.user.shard.MUC_NICKNAME=$DOMAIN
 " > /etc/jitsi/videobridge/sip-communicator.properties
 
 echo "===================> Install jicofo <==================="
-
 cd ~
 
-if [ ! -f ~/jicofo_1.0-508-1_amd64.deb ]
+if [ ! -f /etc/jitsi/jicofo ]
 then
-  wget https://download.jitsi.org/stable/jicofo_1.0-508-1_amd64.deb
-  sudo dpkg -i jicofo_1.0-508-1_amd64.deb
+  apt-get install -y jicofo
 fi
 
 echo "===================> JICOFO change config file <==================="
@@ -242,6 +194,53 @@ sed -i "/JICOFO_AUTH_PASSWORD=/c\JICOFO_AUTH_PASSWORD=$PROSODYPASSWORD" /etc/jit
 
 echo "===================> JICOFO change sip-communicator.properties  <==================="
 echo "org.jitsi.jicofo.BRIDGE_MUC=JvbBrewery@internal.auth.$DOMAIN" > /etc/jitsi/jicofo/sip-communicator.properties
+
+if [ ! -f $PROSODYCONFIGPATH ]
+then
+    sudo cp "$INSTALLPATH/$DOMAIN/doc/debian/jitsi-meet-prosody/prosody.cfg.lua-jvb.example" /etc/prosody/conf.avail/$DOMAIN.cfg.lua
+
+    sudo sed -i "s/plugin_paths = { \"\/usr\/share\/jitsi-meet\/prosody-plugins\/\" }/plugin_paths = { \"\/srv\/$DOMAIN\/resources\/prosody-plugins\/\" }/g" $PROSODYCONFIGPATH
+    sudo sed -i "s/__turnSecret__/$PROSODYPASSWORD/g" $PROSODYCONFIGPATH
+    sudo sed -i "s/focusSecret/$PROSODYPASSWORD/g" $PROSODYCONFIGPATH
+    sudo sed -i "s/focusUser/focus/g" $PROSODYCONFIGPATH
+    sudo sed -i "s/jitmeet.example.com/$DOMAIN/g" $PROSODYCONFIGPATH
+
+    sudo ln -s $PROSODYCONFIGPATH /etc/prosody/conf.d/$DOMAIN.cfg.lua
+
+    echo "===================> Prosody generate keys for domain $DOMAIN and restart <==================="
+
+    sudo prosodyctl cert generate $DOMAIN
+    sudo prosodyctl cert generate "auth.$DOMAIN"
+
+    ln -sf /var/lib/prosody/$DOMAIN.* /etc/prosody/certs/
+    ln -sf /var/lib/prosody/auth.$DOMAIN.* /etc/prosody/certs/
+
+    ln -sf /var/lib/prosody/auth.$DOMAIN.crt /usr/local/share/ca-certificates/auth.$DOMAIN.crt
+    update-ca-certificates -f
+    prosodyctl register focus auth.$DOMAIN $PROSODYPASSWORD
+    prosodyctl register jvb auth.$DOMAIN $PROSODYPASSWORD
+
+    service prosody restart
+    prosodyctl start
+fi
+
+echo "===================> Configure nginx file with domain: $DOMAIN <==================="
+if [ ! -f /etc/nginx/sites-available/$DOMAIN.conf ]
+then
+    cp -rf "$INSTALLPATH/$DOMAIN/doc/debian/jitsi-meet/jitsi-meet.example" $NGINXCONFIGPATH
+
+    sed -i "s/ssl_certificate \/etc\/jitsi\/meet\/jitsi-meet.example.com.crt;/ssl_certificate \/etc\/prosody\/certs\/$DOMAIN.crt;/g" $NGINXCONFIGPATH
+    sed -i "s/ssl_certificate_key \/etc\/jitsi\/meet\/jitsi-meet.example.com.key;/ssl_certificate_key \/etc\/prosody\/certs\/$DOMAIN.key;/g" $NGINXCONFIGPATH
+    sed -i "s/server_names_hash_bucket_size 64;/#server_names_hash_bucket_size 64;/g" $NGINXCONFIGPATH
+    sed -i "s/jitsi-meet.example.com/$DOMAIN/g" $NGINXCONFIGPATH
+    sed -i "s/\/usr\/share\/jitsi-meet/\/srv\/$DOMAIN/g" $NGINXCONFIGPATH
+    sed -i "s/\/etc\/jitsi\/meet/\/srv\/$DOMAIN/g" $NGINXCONFIGPATH
+    sed -i "s/libs\/external_api.min.js;/modules\/API\/external\/external_api.js;/g" $NGINXCONFIGPATH
+#    sed -i "s/# server_names_hash_bucket_size 64;/server_names_hash_bucket_size 64;/gi" $NGINXCONFIGPATH
+
+    sudo ln -s $NGINXCONFIGPATH /etc/nginx/sites-enabled/$DOMAIN.conf
+    sudo systemctl restart nginx
+fi
 
 echo "===================> Update current jitsi from git <==================="
 
@@ -256,6 +255,6 @@ sudo make & wait
 sudo systemctl restart nginx
 sudo service prosody restart
 sudo service jicofo restart
-sudo service jitsi-videobridge restart
+sudo service jitsi-videobridge2 restart
 
 echo "===================> Installation finished <==================="
