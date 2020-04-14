90a7e44acb29c1c43f3495b833534a3c8444811f
Vasyl B <xrescon@gmail.com>
2020-04-14 11:06:36 +0300

    Change show tile after 1 sec.

--- modules/UI/videolayout/VideoLayout.js
+++ modules/UI/videolayout/VideoLayout.js
@@ -311,7 +311,7 @@ const VideoLayout = {
                 this.updateMutedForNoTracks(id, 'audio');
                 this.updateMutedForNoTracks(id, 'video');
             }
-        }, 300);
+        }, 1000);
     },
 
     /**
