2acaeed1ae210c734bc566a3bdc9aa28a3a51c54
Vasyl B <xrescon@gmail.com>
2020-05-13 18:34:04 +0300

    [Hotfix] Small fix installation script.

--- installation/configure_jitsi.sh
+++ installation/configure_jitsi.sh
@@ -5,7 +5,7 @@ DOMAIN=$2
 
 echo "Start jitsi configuration"
 cd $INSTALLPATH/$DOMAIN
-cp -rf ./choop.chat-config.js "./$DOMAIN-config.js"
+cp -rf ./config.js "./$DOMAIN-config.js"
 sed -i "s/domain: 'choop.chat',/domain: '$DOMAIN',/gi" "./$DOMAIN-config.js"
 sed -i "s/bridge: 'jitsi-videobridge.choop.chat',/\/\/ bridge: 'jitsi-videobridge.$DOMAIN',/gi" "./$DOMAIN-config.js"
 sed -i "s/muc: 'conference.choop.chat'/muc: 'conference.$DOMAIN'/gi" "./$DOMAIN-config.js"
