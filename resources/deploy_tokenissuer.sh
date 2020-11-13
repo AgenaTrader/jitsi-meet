08708ee3befac9f0dd29d11ccdda479f65843d24
Sylwester Kardziejonek <sylwester.kardziejonek@gmail.com>
2020-11-13 15:44:00 +0100

    Change ChoopChat.TokenIssuer clone url. Use new GitLab token without password.

--- resources/deploy_tokenissuer.sh
+++ resources/deploy_tokenissuer.sh
@@ -3,7 +3,7 @@ systemctl stop choopchat.tokenissuer.service
 mkdir /var/www/ChoopChat.TokenIssuer.Src
 cd /var/www/ChoopChat.TokenIssuer.Src
 # We have moved to GitLab, so we need t
-git clone https://gitlab+deploy-token-1:DyPcepn9cGHs1un_Uxu9@repo.agenatrader.com/agenatrader-projects/choopchat.tokenissuer.git ./ --single-branch --branch develop
+git clone https://gitlab+deploy-token-2@repo.agenatrader.com/agenatrader-projects/choopchat.tokenissuer.git ./ --single-branch --branch develop
 dotnet publish ChoopChat.TokenIssuer.sln --output=/var/www/build --configuration=Release
 dotnet clean
 cd ..
